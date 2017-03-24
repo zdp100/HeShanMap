b6b11b8cca497b5dbff1e4e107508d25ca59b5d4
zdp100 <zdp100@gmail.com>
2017-03-25 01:48:45 +0800

    2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace maphtml.Controllers
+namespace HeShanMap.Controllers
 {
     public class HomeController : Controller
     {
--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace maphtml
+namespace HeShanMap
 {
     public class Startup
     {
